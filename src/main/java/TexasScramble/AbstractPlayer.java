9ce7ab729f6405b46857fb9c934a7acfb522c8d1
Murnane1 <conor.murnane@ucdconnect.ie>
2023-04-13 16:37:41 +0100

    player layout

---
+++ src/main/java/TexasScramble/AbstractPlayer.java
@@ -0,0 +1,4 @@
+package src.main.java.TexasScramble;
+
+public class AbstractPlayer implements Player {
+}
---
+++ src/main/java/TexasScramble/ComputerScramblePlayer.java
@@ -0,0 +1,4 @@
+package src.main.java.TexasScramble;
+
+public class ComputerScramblePlayer extends AbstractPlayer {
+}
