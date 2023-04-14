68d871e375ff9dd9a5c0d314004b76ebd60d86c5
Murnane1 <conor.murnane@ucdconnect.ie>
2023-04-14 13:35:08 +0100

    added Tile class

---
+++ src/main/java/TexasScramble/Tile.java
@@ -0,0 +1,19 @@
+package texasScramble;
+
+public class Tile {
+    private char letter;
+    private int value;
+
+    public Tile(char letter, char value){
+        this.letter = letter;
+        this.value = value;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
