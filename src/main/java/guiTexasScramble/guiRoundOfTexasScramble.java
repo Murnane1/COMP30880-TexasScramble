a31e665071cdefc03307eae311491d9ae36fc83b
dhruvg102 <dhruv.gupta1500@gmail.com>
2023-04-21 22:15:02 +0100

    Linking Game Loop with GUI

--- src/main/java/guiTexasScramble/guiRoundOfTexasScramble.java
+++ src/main/java/guiTexasScramble/guiRoundOfTexasScramble.java
@@ -3,18 +3,28 @@ package guiTexasScramble;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+
 import Entities.*;
+import GUI.PauseButton;
+import GUI.PlayButton;
 import texasScramble.*;
+import java.awt.event.MouseEvent;
 
 public class guiRoundOfTexasScramble extends RoundOfTexasScramble{
 
+
+
     private ArrayList<guiPlayer> guiPlayers = new ArrayList<>();
+
+    public boolean flag = false;
 
     public guiRoundOfTexasScramble(BagOfTiles bag, Player[] players, int smallBlind, int button) {
         super(bag, players, smallBlind, button);
 
         init(players);
     }
+
+ 
 
     private void init(Player[] players) {
         int i = 100;
@@ -27,31 +37,102 @@ public class guiRoundOfTexasScramble extends RoundOfTexasScramble{
             } 
             else{
                 guiPlayers.add(new guiComputerScramblePlayer(i,j,player));
-                j+=50;
+                i+=70;
+                j+=70;
+            }
+        }
+    }
+    
+
+    public void update(){
+        for(guiPlayer player: guiPlayers){
+            if(player.getClass()==guiHumanScramblePlayer.class){
+                guiHumanScramblePlayer Hplayer = (guiHumanScramblePlayer)player;
+                Hplayer.update();
+            }else{
+                guiComputerScramblePlayer Cplayer = (guiComputerScramblePlayer)player;
+                Cplayer.update(); 
+            }
+        }
+      
+    }
+    public void draw(Graphics g){
+        //TODO DRAW BUTTON (YES AND NO)??
+
+        for(guiPlayer player: guiPlayers){
+            if(player.getClass()==guiHumanScramblePlayer.class){
+                guiHumanScramblePlayer Hplayer = (guiHumanScramblePlayer)player;
+                Hplayer.render(g);
+            }else{
+                guiComputerScramblePlayer Cplayer = (guiComputerScramblePlayer)player;
+                Cplayer.render(g); 
+            }
+        }
+      
+    }  
+    @Override
+    public void bettingCycle(PotOfMoney mainPot, int playerStart) {
+        int stake = -1;
+        int numActive = mainPot.getNumPlayers();
+
+        while (stake < mainPot.getCurrentStake() && numActive > 1) {
+
+            stake = mainPot.getCurrentStake();
+
+            for (int i = 0; i < getNumPlayers(); i++) {
+                Player currentPlayer = mainPot.getPlayer((playerStart + i) % mainPot.getNumPlayers());
+
+                if (currentPlayer == null || currentPlayer.hasFolded() || currentPlayer.isAllIn()) {
+                    continue;
+                }
+
+                delay(DELAY_BETWEEN_ACTIONS);
+
+                int index = 0;
+                for(index = 0; i< players.length;i++){
+                    if(currentPlayer==players[i]){
+                        break;
+                    }
+                }
+
+                guiPlayers.get(index).nextAction(mainPot);
+                // currentPlayer.nextAction(mainPot);
+
+                //actions after player's move
+                if (currentPlayer.isAllIn() || currentPlayer.hasFolded()) {
+                    numActive--;
+                }
+            }
+        }
+        flag = true;
+    }
+    
+    public void mousePressed(MouseEvent e) {
+        for(guiPlayer player: guiPlayers){
+            if(player.getClass()==guiHumanScramblePlayer.class){
+                guiHumanScramblePlayer Hplayer = (guiHumanScramblePlayer)player;
+                Hplayer.mousePressed(e);
             }
         }
     }
 
-    public void update(){
-
-    }
-    public void draw(Graphics g){
-        //TODO DRAW BUTTON (YES AND NO)??
+    public void mouseReleased(MouseEvent e) {
         for(guiPlayer player: guiPlayers){
-            player.render(g);
+            if(player.getClass()==guiHumanScramblePlayer.class){
+                guiHumanScramblePlayer Hplayer = (guiHumanScramblePlayer)player;
+                Hplayer.mouseReleased(e);
+            }
         }
-    }  
+    }
 
-    // @Override
-    // public void printPlayerHand(){
-    //     //TODO Draw hand 
-
-    // }
-
-    // @Override
-    // public void bettingCycle(PotOfMoney pot, int playerStart){
-    //     //TODO
-    // }
+    public void mouseMoved(MouseEvent e) {
+        for(guiPlayer player: guiPlayers){
+            if(player.getClass()==guiHumanScramblePlayer.class){
+                guiHumanScramblePlayer Hplayer = (guiHumanScramblePlayer)player;
+                Hplayer.mouseMoved(e);
+            }
+        }
+    }
 
     public ArrayList<guiPlayer> getGuiPlayers(){
         return guiPlayers;
